5c57eb32a73feef472a39b7ccc63584a44426d86
xan <shengxin.zhuang@sealan.tech>
2024-03-11 10:52:42 +0800

    feat(seanet): 实现获取seanet zigbee设备日志，向网关设备下发控制指令，获取网关设备下所属设备
    Signed-off-by: xan <shengxin.zhuang@sealan.tech>

--- common/base.go
+++ common/base.go
@@ -6,6 +6,15 @@ type BaseResponse struct {
 	Message string `json:"message,omitempty"`
 }
 
+type BaseListResponse struct {
+	Code    int    `json:"code"`
+	Status  bool   `json:"status,omitempty"`
+	Message string `json:"message,omitempty"`
+	Total   int    `json:"total,omitempty"`
+	Current int    `json:"current,omitempty"`
+	Perpage int    `json:"per_page,omitempty"`
+}
+
 type PaginationParams struct {
 	Current int `json:"current"`
 	PerPage int `json:"per_page"`
--- seanet/device.go
+++ seanet/device.go
@@ -11,6 +11,7 @@ import (
 	"github.com/valyala/fastjson"
 )
 
+// 获取设备详情
 func (c *SeanetClient) GetDevice(sn string) (*dto.Device, error) {
 	uri := fmt.Sprintf(apiModifyDevice, sn)
 	body, err := c.SendHttpRequest(uri, http.MethodGet, nil)
@@ -21,6 +22,7 @@ func (c *SeanetClient) GetDevice(sn string) (*dto.Device, error) {
 	return c.responseDevice(body)
 }
 
+// 更新设备
 func (c *SeanetClient) UpdateDevice(dto *dto.UpdateDeviceDto) (*common.BaseResponse, error) {
 	if err := c.validate.Struct(dto); err != nil {
 		return nil, err
@@ -40,6 +42,7 @@ func (c *SeanetClient) UpdateDevice(dto *dto.UpdateDeviceDto) (*common.BaseRespo
 	return &result, nil
 }
 
+// 获取设备列表
 func (c *SeanetClient) ListDevice(pagination common.PaginationParams) (deviceListResp dto.ListDeviceDto, err error) {
 	body, err := c.SendHttpRequest(apiDevice, http.MethodGet, pagination)
 	if err != nil {
@@ -73,6 +76,7 @@ func (c *SeanetClient) ListDevice(pagination common.PaginationParams) (deviceLis
 	return
 }
 
+// 控制设备
 func (c *SeanetClient) SwitchDevice(param *dto.SwitchDeviceDto) (resp *common.BaseResponse, err error) {
 	if err := c.validate.Struct(param); err != nil {
 		return nil, err
@@ -114,4 +118,104 @@ func (c *SeanetClient) responseDevice(b []byte) (*dto.Device, error) {
 	}
 
 	return &resultDevice, nil
+}
+
+// 获取zigbee设备日志
+func (c *SeanetClient) GetDeviceLog(sn string, pagination common.PaginationParams) (deviceLogsResp dto.DeviceLogDto, err error) {
+	uri := fmt.Sprintf(apiLogDevice, sn)
+	body, err := c.SendHttpRequest(uri, http.MethodGet, pagination)
+	if err != nil {
+		return deviceLogsResp, err
+	}
+
+	var p fastjson.Parser
+	v, err := p.Parse(string(body))
+	if err != nil {
+		return
+	}
+
+	if !v.GetBool("status") {
+		msg := v.GetStringBytes("message")
+		if err != nil {
+			return
+		}
+		err = errors.New(string(msg))
+		return
+	}
+
+	err = json.Unmarshal(body, &deviceLogsResp)
+	if err != nil {
+		return
+	}
+
+	if !deviceLogsResp.Status {
+		err = fmt.Errorf("code : %d", deviceLogsResp.Code)
+	}
+
+	return
+}
+
+// 获取zigbee网关下的设备列表
+func (c *SeanetClient) ListGatewayDevice(gatewaySn string) (BelongDevicesResp dto.BelongDeviceDto, err error) {
+	uri := fmt.Sprintf(apiBelongDevices, gatewaySn)
+	body, err := c.SendHttpRequest(uri, http.MethodGet, nil)
+	if err != nil {
+		return
+	}
+
+	var p fastjson.Parser
+	v, err := p.Parse(string(body))
+	if err != nil {
+		return
+	}
+
+	if !v.GetBool("status") {
+		msg := v.GetStringBytes("message")
+		if err != nil {
+			return
+		}
+		err = errors.New(string(msg))
+		return
+	}
+
+	err = json.Unmarshal(body, &BelongDevicesResp)
+	if err != nil {
+		return
+	}
+
+	if !BelongDevicesResp.Status {
+		err = fmt.Errorf("code : %d", BelongDevicesResp.Code)
+	}
+
+	return
+}
+
+const (
+	Forget     = "ZbForget"
+	Restart    = "Restart"
+	PermitJoin = "ZbPermitJoin"
+)
+
+// 控制网关设备
+func (c *SeanetClient) ControlGatewayDevice(devicekey, cmd, content string) (resp *common.BaseResponse, err error) {
+	var param dto.CmdDeviceDto
+	device, err := c.GetDevice(devicekey)
+	if err != nil {
+		return nil, err
+	}
+
+	param.Topic = fmt.Sprintf("/%s/%s/user/cmnd/%s", device.ProductKey, device.Sn, cmd)
+	param.Content = content
+
+	body, err := c.SendHttpRequest(apiCmdDevice, http.MethodPost, param)
+	if err != nil {
+		return nil, err
+	}
+
+	var result common.BaseResponse
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
 }
